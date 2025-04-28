ae8620b410393b408ec0024025742462d533a323
RazaDon Willrich <razradical93@gmail.com>
2025-04-28 06:30:20 +0000

    Completed Reverse A String.

--- src/main/java/StringReverse.java
+++ src/main/java/StringReverse.java
@@ -13,6 +13,14 @@ public class StringReverse {
      * @return the reverse of str.
      */
     public String reverse(String str){
-        return null;
+    
+        if (str == null) {
+            return null;
+        }
+        StringBuilder flipStr = new StringBuilder();
+        for (int i = str.length() - 1; i >= 0; i--) {
+            flipStr.append(str.charAt(i));
+        }
+        return flipStr.toString();
     }
 }
